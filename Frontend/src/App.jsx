5620fc7ff02b72e47ea39d261ba1b064e91895df
prathameshcodes03 <prathamesh.zone09@gmail.com>
2026-06-01 15:21:55 +0530

    Initial React project setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import react from 'react'
+
+
+function App() {
+  
+
+  return (
+   <div className='bg-black h-screen text-white text-center pt-[27%] '>
+    Hello
+   </div>
+  )
+}
+
+export default App
